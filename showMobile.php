c2236a4a2a9ebcfe700d4b188911d4cabb7ac885
josephstalin <lyz2356002@gmail.com>
2015-01-20 15:24:57 +0800

    add image module

--- showMobile.php
+++ showMobile.php
@@ -2,16 +2,21 @@
  include 'db.php';
  $id = $_GET['id'];
  $db->set_charset("utf8");
- $query_pag_data = "SELECT title,tag,time,url,content,likes from news where id=$id";
+ $query_pag_data = "SELECT title,tag,time,url,content,likes,image from news where id=$id";
  $result_pag_data = $db->query($query_pag_data);
  $row = mysqli_fetch_row($result_pag_data);
+ $img="";
  $title = $row[0];
  $tag = $row[1];
  $time = date('Y-m-d', strtotime($row[2]));
  $url = $row[3];
  $content = $row[4];
  $likes=$row[5];
-
+ $imageUrl=$row[6];
+ echo $imageUrl;
+ if($imageUrl!=""){
+    $img="<img src=".$imageUrl." style='max-height: 200px; max-width: 300px;'>";
+ }
 ?>
 <!DOCTYPE html>
 <html>
@@ -44,6 +49,7 @@
                 <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-tag ui-btn-icon-left "><?php echo $tag?></a>
             </div>
             <div data-role="content">
+                <div><?php echo $img?></div>
                 <p><?php echo $content?></p>
             </div>
             <div data-role="footer">
